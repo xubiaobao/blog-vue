bc98104ba832414f2e9929a73e02d97f421c9acb
641804809 <641804809@qq.com>
2023-05-17 22:20:50 +0800

    看板娘

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -16,6 +16,41 @@ import mouseMove from '../components/mouseMove.vue'
 export default{
   name: 'Layout',
   components: { AppMain, mouseMove, Skip, Date },
+  /**
+   * 看板娘初始化
+    参数说明
+    model 模型的配置
+       json 文件资源路径，可以支持网络路径，此处使用的是相对路径
+       scale 模型缩放系数
+    display 模型布局
+       position 位置
+       width 宽度
+       height 高度
+       hOffset 水平偏移量
+       vOffset 垂直偏移量
+    mobile 移动端配置
+       show 显示/隐藏
+       scale 缩放比例
+    react 模型矩形框样式
+       opacity 透明度
+       opacityDefault 默认透明度
+       opacityOnHover 鼠标悬浮透明度
+  */
+  created () {
+    setTimeout(() => {
+      window.L2Dwidget.init({
+        pluginRootPath: '../../static/live2dw/',
+        pluginJsPath: 'lib/',
+        pluginModelPath: 'live2d-widget-model-haru_2/assets/', // 中间这个haru_2就是看板娘,换这个就可以了
+        tagMode: false,
+        debug: false,
+        model: { jsonPath: '../../static/live2dw/live2d-widget-model-haru_2/assets/haru02.model.json' },
+        display: { position: 'left', width: 150, height: 300 }, // 调整大小,和位置
+        mobile: { show: true }, // 要不要盯着你的鼠标看
+        log: false
+      })
+    }, 1000)
+  },
   mounted () {
     window.addEventListener('wheel', this.handleMouseWheel, {
       passive: false
