d36e19dc6705ab5c904e3c51c7d58ecb6d1003bd
641804809 <641804809@qq.com>
2023-05-29 18:03:28 +0800

    1

--- src/view/Board.vue
+++ src/view/Board.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>留言板</div>
+  <div>
+    <textarea resize="none" rows="15" style="width: 550px;position: relative;z-index: 1;" />
+    <el-button class="filter-item" type="primary" @click="addWhite">留言</el-button>
+  </div>
 </template>
 
 <script>
 export default ({
-  name: 'Board'
+  name: 'Board',
+  methods: {
+    addWhite () {
+      console.log('aaa')
+    }
+  }
 })
 </script>
 
